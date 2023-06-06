1592c6b04fd90bd97aee2cdf27669f296b8d3430
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2023-06-06 23:13:26 +0500

    remove api

--- api/controllers/userController.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-import { User } from '../models/index.js';
-
-export function getUserProfile(req,res,next){
-    res.status(200).json({
-        success:true,
-        msg:req.user
-    })
-}
--- api/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { stripePayment } from '../controllers/orderController.js';
-
-
-router.post("/order",stripePayment);
-
-export default router;
