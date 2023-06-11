d8f3b5f0ce088926a065ccd254e2fd5ba1412d95
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2023-06-11 20:38:05 +0500

    handle sidebars z index

--- src/components/CartMenu.jsx
+++ src/components/CartMenu.jsx
@@ -31,7 +31,7 @@ function CartMenu() {
 
   return (
     // modal screen
-    <div className={`${isCartOpen?"block":"hidden"} bg-black/40 fixed z-30 w-full h-full left-0 top-0 overflow-auto scrollbar`}>
+    <div className={`${isCartOpen?"block":"hidden"} bg-black/40 fixed z-[99999] w-full h-full left-0 top-0 overflow-auto scrollbar`}>
         {/* <div className='hidden md:block md:w-[70%] md:fixed md:top-0 md:bottom-0 md:left-0' onClick={()=>dispatch(setIsCartOpen({}))}></div> */}
         {/* Cart Sidebar  */}
         <motion.div initial={{opacity:0,scaleX:0,x:150}} transition={{type:"spring",duration:0.8,bounce:0.4}} whileInView={{opacity:1,scaleX:1,x:0}} viewport={{ once: false }}  className="fixed right-0 top-0 w-full md:w-[max(400px,30%)] h-full bg-white">
